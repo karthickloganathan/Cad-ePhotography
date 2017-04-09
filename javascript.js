e16a28d755254413baf00f713af01ff6e93ab0bf
karthickloganathan <kartilogan@gmail.com>
2017-04-09 10:14:51 +0530

    Add files via upload
    website files

---
+++ javascript.js
@@ -0,0 +1,24 @@
+var index= 1;
+autoSlide();
+
+function autoSlide(){
+    
+var i;
+var x=document.getElementsByClassName("slide");
+
+
+
+
+
+for(i=0;i<x.length;i++){
+    x[i].style.display="none";
+}    
+    
+    if(index >x.length){index=1};
+    x[index-1].style.display="block";
+    index++;
+    
+  setTimeout(autoSlide,2000);  
+    
+}
+
